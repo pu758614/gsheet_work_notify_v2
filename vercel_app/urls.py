7834af6be51b841ea5d7950efe3a2fd0dc302458
shiun <pu758614@gmail.com>
2024-02-05 01:11:56 +0800

    update

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from example import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example.urls')),
+    path('api/lineCallback', views.lineCallback)
 ]
