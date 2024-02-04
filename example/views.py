416a361531df1fbbf0e4bbd8759c3401770ceccd
shiun <pu758614@gmail.com>
2024-02-05 01:01:59 +0800

    update

--- example/views.py
+++ example/views.py
@@ -27,7 +27,7 @@ def index(request):
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel!</h1>
+            <h1>aaaaaaaaaHello from Vercel!</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
